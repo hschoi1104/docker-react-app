d2708cb500ed4b78d6e5bad93fd5da759b1ac483
hschoi1104 <hschoi1104@gmail.com>
2020-10-02 17:29:42 +0900

    second push

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 안녕하세요ㅓㅓㅓㅓ
+          Learn react
         </a>
       </header>
     </div>
